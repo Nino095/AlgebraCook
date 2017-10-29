3ad09e7fa68f1b9ddbbf1e6b6d9afa6170e907f0
unknown <ninobicanic7@gmail.com>
2017-10-29 19:49:48 +0100

    napravljen RecipesController

---
+++ app/Http/Controllers/RecipesController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class RecipesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return ("Popis svih recepata");
+    }
+
+	
+	
+	
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // 
+    }
+	
+    public function add()
+    {
+        return ('prikaz view-a sa web obrascem za unos.');
+    }
+	
+	public function save(Request $request)
+	{
+		// $request->nameInputa = vrijednostInputa
+		return ("Spremam podatke iz webobrasca u bazu");
+	}
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function view($id)
+    {
+        return "Prikaz recepta ID: " . $id;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return "Edit recepta sa ID: " . $id; // Prikaz web obrasca
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        return "Spremanje promjena recepta ID: " . $id;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function delete($id)
+    {
+        return "Brisanje recepta ID: " . $id;
+    }
+}
