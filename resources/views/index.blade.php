2af3626499758470a2a867f0edc486c7ebb215a1
unknown <ninobicanic7@gmail.com>
2017-10-30 12:43:16 +0100

    index i errors view dodani

---
+++ resources/views/index.blade.php
@@ -0,0 +1,29 @@
+@extends('master')
+
+@section('title', 'Recepti')
+
+@section('content')
+<div class="container">
+	<div class="col-md-10-offset-1">
+		<div class="panel-default">
+			<div class="panel-heading">Recepti<br>
+				<a href="{{ url ('/recipes/add') }}">
+					<i class="fa fa-btn fa-plus"></i>
+					Dodaj novi recept
+				</a>
+				</div>
+				<div class="panel-body">
+					<ul class="list-group">
+						@foreach ($recipes as $recipe)
+						<li class="list-group-item">
+							<a href="recipes/view/{{ $recipes->id }}">{{ $recipe->name }}</a>
+						</li>
+						
+						
+						@endforeach
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+@endsection
