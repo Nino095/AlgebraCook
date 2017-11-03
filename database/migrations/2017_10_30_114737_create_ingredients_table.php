01535b8741c5855ab4d02186852e65fcc4270570
unknown <ninobicanic7@gmail.com>
2017-11-03 16:52:06 +0100

    ..

--- database/migrations/2017_10_30_114737_create_ingredients_table.php
+++ database/migrations/2017_10_30_114737_create_ingredients_table.php
@@ -35,3 +35,40 @@ class CreateIngredientsTable extends Migration
         Schema::drop('ingredients');
     }
 }
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIngredientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ingredients', function (Blueprint $table) {
+            $table->increments('id');
+			$table->string('name');
+			$table->integer('recipe_id')->unsigned();
+            $table->timestamps();
+			
+			$table
+				->foreign('recipe_id')
+				->references('id')->on('recipes')
+				->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('ingredients');
+    }
+}
